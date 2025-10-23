a34abb35e45e163bbb73006c2313d4474893ebe3
scchamberlai <sebastian.chamberlain@student.unimelb.edu.au>
2025-10-23 20:03:01 +1100

    Multiple buttons that scale to device width

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function greet(){
     alert("HFOQNDOQ!!");
 }
+
+function first(){
+    alert("first");
+}
+
+function second(){
+    alert("second");
+}
+
+function third(){
+    alert("third");
+}
+
+function fourth(){
+    alert("fourth");
+}
+
+function fifth(){
+    alert("fifth");
+}
