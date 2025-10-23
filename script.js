843cb3867d36d1a76d3114682cdfec55bbf15eed
scchamberlai <sebastian.chamberlain@student.unimelb.edu.au>
2025-10-23 20:45:55 +1100

    More button work

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ function greet(){
 }
 
 function first(){
-    alert("first");
+    alert("HA!");
 }
 
 function second(){
-    alert("second");
+    alert("Imagine a bunch of posters came up and you could click one and it would let you download the film in question");
 }
 
 function third(){
-    alert("third");
+    alert("#STAYTUNED");
 }
 
 function fourth(){
-    alert("fourth");
+    alert("EMAIL people@hfoqndoq.com");
 }
 
 function fifth(){
-    alert("fifth");
+    window.open("http://100.101.126.7:8096/web/#/movies.html?topParentId=f137a2dd21bbc1b99aa5c0f6bf02a805&collectionType=movies", "_blank")
 }
