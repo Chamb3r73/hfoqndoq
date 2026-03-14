ab420e17cb973a6d3aa4998eeb79393f1c1a419c
scchamberlai <sebastian.chamberlain@student.unimelb.edu.au>
2026-03-14 14:34:47 +1100

    Position the buttons and add basic functionality to redirect to subpages

--- works/script.js
+++ works/script.js
@@ -1,1 +1,16 @@
 // JavaScript components for HFOQNDOQ
+document.getElementById("blog").onclick = function () {
+    window.location.href = "../blog/index.html";
+};
+
+document.getElementById("works").onclick = function () {
+    window.location.href = "../works/index.html";
+};
+
+document.getElementById("photography").onclick = function () {
+    window.location.href = "../photography/index.html";
+};
+
+document.getElementById("contact").onclick = function () {
+    window.location.href = "../contact/index.html";
+};
